df1b0c335c3f3a0cbf3ec2307897ef97834ad200
Mahy Clément <210305@CCM-Asty-Moulin.local>
2023-04-18 12:16:06 +0200

    Râté

---
+++ IMATP5_MahyClement/TrianglePascal/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TrianglePascal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n; //Nombre de lignes et de colonnes pour la matrice
+            string stringM; //Matrice convertie en string
+            sousProgrammes sousProgrammes = new sousProgrammes();
+            do//Permet de donner une valeur supérieure à 0 à n
+            {
+                Console.WriteLine("Que souhaitez vous comme dimension ?");
+                int.TryParse(Console.ReadLine(), out n);
+            } while (n < 1);
+
+            int[,] M = new int[n, n]; //Matrice ayant n comme nombre de ligne et colonne
+
+            sousProgrammes.GenerationUns(n, ref M);//Génère les 1 de la première colonne et en diagonale
+            sousProgrammes.CalculCases(n, ref M);//Calcule l'addition de la valeur au dessus et en haut à gauche
+            sousProgrammes.Concatenation(n, M, out stringM);//Convertie la matrice en string pour pouvoir l'afficher
+
+            Console.Write(stringM);//Affiche la matrice
+        }
+    }
+}
---
+++ IMATP5_MahyClement/TrianglePascal/sousProgrammes.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrianglePascal
+{
+    public struct sousProgrammes
+    {
+        public void GenerationUns(int n, ref int[,] M) //Génère les 1 de la première colonne et en diagonale
+        {
+            for(int i = 0; i < n; i++)
+            {
+                M[i, i] = 1;
+                M[i, 0] = 1;
+            }
+        }
+
+        public void CalculCases(int n, ref int[,] M)//Calcule l'addition de la valeur au dessus et en haut à gauche
+        {
+            for(int i = 1; i < n; i++)
+            {
+                for(int j = 2; j < n; j++)
+                {
+                    int k = M[i - 1, j - 1];
+                    int l = M[i, j - 1];
+                    M[i, j] = k + l;
+                }
+            }
+        }
+
+        public void Concatenation(int n, int[,] M, out string stringM)//Convertie la matrice en string pour pouvoir l'afficher
+        {
+            stringM = "";
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; i++)
+                {
+                    stringM += M[i, j] + " "; //Je pense avoir confondu dans quel sens vont les colonnes et les lignes
+                }
+                stringM += "\n";
+            }
+        }
+    }
+}
